b933e574d64ebaac7da53650fa5017f7193442cd
jwlsiu <114306901+jwlsiu@users.noreply.github.com>
2023-04-15 14:03:46 +0100

    Update python_basics.py

--- python_basics.py
+++ python_basics.py
@@ -1,15 +1,22 @@
 # Python Basics - Activity 1
+# Write a Python program to print the result of the following operations.
+# Declare variables a, b, c, d
 a = -5 + 8 * 6
 b = (55 + 9) % 9
 c = 20 + -3 * 5 / 8
 d = 5 + 15 / 3 * 2 - 8 % 3
+# Print variables to show result
 print(a)
 print(b)
 print(c)
 print(d)
 
 # Python Basics - Activity 2
+# Write a Python program to divide two numbers, using the input from the user, and print the result on the screen.
+# Declare two number variables from user input
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
+# Declare 'divide' variable as first number divided by the second number
 divide = int(first_number / second_number)
+# Print result of 'divide' variable
 print(f"The division of the first number and the second number is: {divide}")
